0a18b5a5fceb2001d7dcca644a04d64ca788b15b
Maryam Shakeel <157610901+maryamshakeel22@users.noreply.github.com>
2024-11-09 12:49:31 +0500

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -78,12 +78,13 @@ const Contact = () => {
                 className="peer block min-h-[auto] w-full rounded border-2 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none "
                 id="exampleFormControlTextarea1"
                 rows={3}
+                name='message'
                 defaultValue={""} required placeholder="Message"
               />
               <label
-                htmlFor="message"
                 className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none "
-              >
+                htmlFor="message"
+                >
                 
               </label>
             </div>
